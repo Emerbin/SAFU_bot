3e1c50851a0cd23e660214f39ed4b9ff037e01a7
NPlesh <plesh204@gmail.com>
2023-11-18 00:03:33 +0200

    Added environment variable for token

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,4 @@ int main() {
     std::string token(tokenenv.begin(), tokenenv.end());
     cBot c(token);
     return 0;
-/*
-
-    bot.getEvents().onCommand("create", [&bot, &creatingBlock,&messagesToEdit, &personalKeyboard](Message::Ptr message)
-        {
-            bot.getApi().sendMessage(message->chat->id, "Choose what block you want to create:", false, 0, personalKeyboard);
-            creatingBlock = true;
-            return;
-        });
-
-    
-   */
 }
